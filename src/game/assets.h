d7a3f30b8041e7023dab0665e413c98e16fb26a8
Rodoft S <rodoft.s@gmail.com>
2025-07-16 00:38:26 +0800

    game: Update assets info and design initial boss room

--- src/game/assets.h
+++ src/game/assets.h
@@ -11,7 +11,10 @@
 // . . . X X X X . . .   . X . X X X X . . .   . . . X X X X . . .
 // . . X . . X . . X X   . . X . . X . . X X   . . X . . X . . X X
 // . . . . X X X X . .   . . . . X X X X . .   . . . . X X X X . .
-// FRAME_SIZE = 16
+#define PLAYER_SHIP_BMP_FRAME_SIZE 16U
+#define PLAYER_SHIP_BMP_FRAME_COUNT 3U
+#define PLAYER_SHIP_BMP_FRAME_WIDTH 10U
+#define PLAYER_SHIP_BMP_FRAME_HEIGHT 8U
 const uint8_t player_ship_bmp[] = {
     // Frame 0
     0xf0, 0x00, 0x24, 0x03, 0x78, 0x00, 0x8c, 0x00, 0x8c, 0x00, 0x78, 0x00, 0x24, 0x03, 0xf0, 0x00,
@@ -34,6 +37,10 @@ const uint8_t player_ship_bmp[] = {
 // . . X X X X X X . X X X
 // . . . X X X X X X X X .
 // . . . . X X X . . . . .
+#define PLAYER_SHIP2_BMP_FRAME_SIZE 26U
+#define PLAYER_SHIP2_BMP_FRAME_COUNT 1U
+#define PLAYER_SHIP2_BMP_FRAME_WIDTH 12U
+#define PLAYER_SHIP2_BMP_FRAME_HEIGHT 13U
 const uint8_t player_ship2_bmp[] = {0x70, 0x00, 0xf8, 0x07, 0x7c, 0x0e, 0x3f, 0x00, 0x7e,
                                     0x00, 0xc5, 0x00, 0x07, 0x01, 0xc5, 0x00, 0x7e, 0x00,
                                     0x3f, 0x00, 0x7c, 0x0e, 0xf8, 0x07, 0x70, 0x00};
@@ -55,6 +62,10 @@ const uint8_t player_ship2_bmp[] = {0x70, 0x00, 0xf8, 0x07, 0x7c, 0x0e, 0x3f, 0x
 // . . . . . . . . . . . X X . . .
 // . . . . . . . . . . . X X . . .
 // . . . . . . . . X X X X X X X .
+#define BOSS_SHIP_BMP_FRAME_SIZE 32U
+#define BOSS_SHIP_BMP_FRAME_COUNT 1U
+#define BOSS_SHIP_BMP_FRAME_WIDTH 16U
+#define BOSS_SHIP_BMP_FRAME_HEIGHT 16U
 const uint8_t boss_ship_bmp[] = {0x00, 0x7f, 0x00, 0x18, 0x00, 0x18, 0x80, 0x7f, 0x00, 0x3f, 0xf0,
                                  0x1f, 0x1c, 0xcf, 0x0f, 0x7e, 0x0f, 0x7e, 0x1c, 0xcf, 0xf0, 0x1f,
                                  0x00, 0x3f, 0x80, 0x7f, 0x00, 0x18, 0x00, 0x18, 0x00, 0x7f};
@@ -67,6 +78,10 @@ const uint8_t boss_ship_bmp[] = {0x00, 0x7f, 0x00, 0x18, 0x00, 0x18, 0x80, 0x7f,
 // . . X X X . .
 // . X . . . X .
 // . . . X . . .
+#define MINE_BMP_FRAME_SIZE 7U
+#define MINE_BMP_FRAME_COUNT 1U
+#define MINE_BMP_FRAME_WIDTH 7U
+#define MINE_BMP_FRAME_HEIGHT 7U
 const uint8_t mine_bmp[] = {0x10, 0x44, 0x38, 0xba, 0x38, 0x44, 0x10};
 
 // 11x15                   11x15
@@ -85,7 +100,10 @@ const uint8_t mine_bmp[] = {0x10, 0x44, 0x38, 0xba, 0x38, 0x44, 0x10};
 // . X . . . . . . X . .   . . X . . . . . . X .
 // X X . . . . . . X . .   . . X . . . . . . X X
 // . . . . . . . . X X .   . X X . . . . . . . .
-// FRAME_SIZE = 30
+#define CRAWLER_BMP_FRAME_SIZE 30U
+#define CRAWLER_BMP_FRAME_COUNT 2U
+#define CRAWLER_BMP_FRAME_WIDTH 11U
+#define CRAWLER_BMP_FRAME_HEIGHT 15U
 const uint8_t crawler_bmp[] = {
     // Frame 0
     0x20, 0x00, 0x20, 0x00, 0x76, 0x00, 0x54, 0x06, 0x74, 0x02, 0x04, 0x02, 0xfc, 0x03, 0xdc, 0x01,
@@ -93,6 +111,75 @@ const uint8_t crawler_bmp[] = {
     // Frame 1
     0x50, 0x00, 0x20, 0x00, 0x70, 0x03, 0x53, 0x01, 0x72, 0x01, 0x02, 0x01, 0xfe, 0x01, 0xac, 0x01,
     0xdc, 0x01, 0xac, 0x01, 0xfc, 0x03, 0x24, 0x02, 0x04, 0x02, 0x04, 0x06, 0x06, 0x00};
+
+// 15x20                             15x20                             15x20
+// . . . . . . X . . . . . . . .     . . . . . . . . . . . . . . .     . . . . . . . . . . . . . . .
+// . . . . . . X . . X . . . . .     . . . X . . . . . . . . . . .     . . . . . . . . . X . . . . .
+// . . . . . X . X . . . . . . .     . X . . X X . . . . . . . . .     . . . . X . . . X . . . . . .
+// . . . X . . X . X . . . . . .     . . . . X . . . . . . . . . .     . . . . . . . X . . . . . . .
+// . . X . . . X X . . . . . . .     . . . X . . X X . . . . . . .     . . . . X . . . X . . . . . .
+// . . . . X . . X X . . . . . .     . X . . X . . . . . . . . . .     . . . . . . . X . . . . . . .
+// . . . . . X X X X X . . . . .     . . . . X X X X X X . . . . .     . . . . . X X X X X . . . . .
+// . . . X X . X X . . X X . . .     . . . X X X X . . . X X . . .     . . . X X . . X . . X X . . .
+// . . X . X . . X X . . . X . .     . . X . . X X X . X . . X . .     . . X . . X X X . X . . X . .
+// . X . . . X X . X . X . . X .     . X . . . . X . . . X . . X .     . X . X . . X . X . . . . X .
+// . X X . . . . X . . . . X X .     . X X . . X . X . . . . X X .     . X X . . . . X . X . . X X .
+// . X . X X . . . . . X X . X .     . X . X X . . . . . X X . X .     . X . X X . . . . . X X . X .
+// . X . . . X X X X X . . . X .     . X . . . X X X X X . . . X .     . X . . . X X X X X . . . X .
+// . . X . . . . . . . . . X . .     . . X . . . . . . . . . X . .     . . X . . . . . . . . . X . .
+// . . X . . . . . X X X . X . .     . . X . . . . . X X X . X . .     . . X . . . . . X X X . X . .
+// . X X X . . . X X . . X X X .     . X X X . . . X X . . X X X .     . X X X . . . X X . . X X X .
+// . X . X X . . . . . X X . X .     . X . X X . . . . . X X . X .     . X . X X . . . . . X X . X .
+// X X . . X X X X X X X . . X X     X X . . X X X X X X X . . X X     X X . . X X X X X X X . . X X
+// X . . . . X . X . X . . . . X     X . . . . X . X . X . . . . X     X . . . . X . X . X . . . . X
+// X . . . X . . X . . X . . . X     X . . . X . . X . . X . . . X     X . . . X . . X . . X . . . X
+//
+// 15x20                             15x20
+// . . . . . . . . . . . . . . .     . . . . . . . . . . . . . . .
+// . . . . . . . . . . . . . . .     . . . . . . . . . . . . . . .
+// . . . . . . . . . . . . . . .     . . . . . . . . . X . . . X .
+// . . . . . . . . . . . . . . .     . . . . . . . . . . . . . . .
+// . . . . . X . . X . . X . . .     . . . . . . X . . . . X . . .
+// . . . . . . . X . . . . . . .     . . . . . . . . . . X . . . .
+// . . . . . X X X X X . . . . .     . . . . . X X X X X X . X . .
+// . . . X X . . X . . X X . . .     . . . X X . X . X X X X . . .
+// . . X . . . . X X X . . X . .     . . X . . . . X X X . . X . .
+// . X . . X . X . X . X . . X .     . X . . X X . . X . . X . X .
+// . X X . . . . X . . . . X X .     . X X . . . X X . . . . X X .
+// . X . X X . . . . . X X . X .     . X . X X . . . . . X X . X .
+// . X . . . X X X X X . . . X .     . X . . . X X X X X . . . X .
+// . . X . . . . . . . . . X . .     . . X . . . . . . . . . X . .
+// . . X . . . . . X X X . X . .     . . X . . . . . X X X . X . .
+// . X X X . . . X X . . X X X .     . X X X . . . X X . . X X X .
+// . X . X X . . . . . X X . X .     . X . X X . . . . . X X . X .
+// X X . . X X X X X X X . . X X     X X . . X X X X X X X . . X X
+// X . . . . X . X . X . . . . X     X . . . . X . X . X . . . . X
+// X . . . X . . X . . X . . . X     X . . . X . . X . . X . . . X
+#define CAULDRON_BMP_FRAME_SIZE 40U
+#define CAULDRON_BMP_FRAME_COUNT 5U
+#define CAULDRON_BMP_FRAME_WIDTH 15U
+#define CAULDRON_BMP_FRAME_HEIGHT 20U
+const uint8_t cauldron_bmp[] = {
+    // Frame 0
+    0x40, 0x00, 0x40, 0x02, 0xa0, 0x00, 0x48, 0x01, 0xc4, 0x00, 0x90, 0x01, 0xe0, 0x03, 0xd8, 0x0c,
+    0x94, 0x11, 0x62, 0x25, 0x86, 0x30, 0x1a, 0x2c, 0xe2, 0x23, 0x04, 0x10, 0x04, 0x17, 0x8e, 0x39,
+    0x1a, 0x2c, 0xf3, 0x67, 0xa1, 0x42, 0x91, 0x44,
+    // Frame 1
+    0x00, 0x00, 0x08, 0x00, 0x32, 0x00, 0x10, 0x00, 0xc8, 0x00, 0x12, 0x00, 0xf0, 0x03, 0x78, 0x0c,
+    0xe4, 0x12, 0x42, 0x24, 0xa6, 0x30, 0x1a, 0x2c, 0xe2, 0x23, 0x04, 0x10, 0x04, 0x17, 0x8e, 0x39,
+    0x1a, 0x2c, 0xf3, 0x67, 0xa1, 0x42, 0x91, 0x44,
+    // Frame 2
+    0x00, 0x00, 0x00, 0x02, 0x10, 0x01, 0x80, 0x00, 0x10, 0x01, 0x80, 0x00, 0xe0, 0x03, 0x98, 0x0c,
+    0xe4, 0x12, 0x4a, 0x21, 0x86, 0x32, 0x1a, 0x2c, 0xe2, 0x23, 0x04, 0x10, 0x04, 0x17, 0x8e, 0x39,
+    0x1a, 0x2c, 0xf3, 0x67, 0xa1, 0x42, 0x91, 0x44,
+    // Frame 3
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x20, 0x09, 0x80, 0x00, 0xe0, 0x03, 0x98, 0x0c,
+    0x84, 0x13, 0x52, 0x25, 0x86, 0x38, 0x1a, 0x2c, 0xe2, 0x23, 0x04, 0x10, 0x04, 0x17, 0x8e, 0x39,
+    0x1a, 0x2c, 0xf3, 0x67, 0xa1, 0x42, 0x91, 0x44,
+    // Frame 4
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x22, 0x00, 0x00, 0x40, 0x08, 0x00, 0x04, 0xe0, 0x17, 0x58, 0x0f,
+    0x84, 0x13, 0x32, 0x29, 0xc6, 0x30, 0x1a, 0x2c, 0xe2, 0x23, 0x04, 0x10, 0x04, 0x17, 0x8e, 0x39,
+    0x1a, 0x2c, 0xf3, 0x67, 0xa1, 0x42, 0x91, 0x44};
 
 // 13x12
 // . . . . . X X X X . . . .
@@ -107,6 +194,10 @@ const uint8_t crawler_bmp[] = {
 // . X X X X X . . . . X X .
 // . . . . . X X . . X . . .
 // . . . . . . . X X . . . .
+#define ASTEROID_BIG_BMP_FRAME_SIZE 24U
+#define ASTEROID_BIG_BMP_FRAME_COUNT 1U
+#define ASTEROID_BIG_BMP_FRAME_WIDTH 13U
+#define ASTEROID_BIG_BMP_FRAME_HEIGHT 12U
 const uint8_t asteroid_big_bmp[] = {0xe0, 0x01, 0xf0, 0x02, 0x18, 0x0f, 0x14, 0x18,
                                     0x07, 0x10, 0x01, 0x10, 0x63, 0x1c, 0x72, 0x0c,
                                     0x02, 0x08, 0x3e, 0x0c, 0x60, 0x02, 0x80, 0x01};
@@ -118,4 +209,8 @@ const uint8_t asteroid_big_bmp[] = {0xe0, 0x01, 0xf0, 0x02, 0x18, 0x0f, 0x14, 0x
 // X . . . X X .
 // . X . . . X .
 // . . X X X . .
+#define ASTEROID_SMALL_BMP_FRAME_SIZE 6U
+#define ASTEROID_SMALL_BMP_FRAME_COUNT 1U
+#define ASTEROID_SMALL_BMP_FRAME_WIDTH 7U
+#define ASTEROID_SMALL_BMP_FRAME_HEIGHT 6U
 const uint8_t asteroid_small_bmp[] = {0x3c, 0x46, 0x69, 0x31, 0x22, 0x1c};
--- src/game/game.c
+++ src/game/game.c
@@ -54,43 +54,45 @@ void game_init()
     ecs.entities.dense = entities_dense;
 
     ecs_entity_t player_ent = ecs_create_entity(&ecs, (PLAYER_TAG | ANIMATE_TAG));
-    ecs_entity_t enemy_ent = ecs_create_entity(&ecs, (ENEMY_TAG | ANIMATE_TAG));
-    ecs_entity_t asteroid1_ent = ecs_create_entity(&ecs, OBSTACLE_TAG);
-    ecs_entity_t asteroid2_ent = ecs_create_entity(&ecs, OBSTACLE_TAG);
-    ecs_entity_t asteroid3_ent = ecs_create_entity(&ecs, OBSTACLE_TAG);
+    ecs_entity_t cauldron_ent = ecs_create_entity(&ecs, (ENEMY_TAG | ANIMATE_TAG));
+    ecs_entity_t crawler1_ent = ecs_create_entity(&ecs, (ENEMY_TAG | ANIMATE_TAG));
+    ecs_entity_t crawler2_ent = ecs_create_entity(&ecs, (ENEMY_TAG | ANIMATE_TAG));
 
-    position_comp_t pos = {.x = 10.f, .y = 32.f};
+    position_comp_t pos = {.x = 30.f, .y = 28.f};
     ecs_add_component(&ecs, player_ent, POSITION_COMP_ID, &pos);
-    pos = (position_comp_t) {.x = 30.f, .y = 32.f};
-    ecs_add_component(&ecs, enemy_ent, POSITION_COMP_ID, &pos);
-    pos = (position_comp_t) {.x = 60.f, .y = 32.f};
-    ecs_add_component(&ecs, asteroid1_ent, POSITION_COMP_ID, &pos);
-    pos = (position_comp_t) {.x = 90.f, .y = 32.f};
-    ecs_add_component(&ecs, asteroid2_ent, POSITION_COMP_ID, &pos);
-    pos = (position_comp_t) {.x = 110.f, .y = 32.f};
-    ecs_add_component(&ecs, asteroid3_ent, POSITION_COMP_ID, &pos);
+    pos = (position_comp_t) {.x = 100.f, .y = 22.f};
+    ecs_add_component(&ecs, cauldron_ent, POSITION_COMP_ID, &pos);
+    pos = (position_comp_t) {.x = 117.f, .y = 5.f};
+    ecs_add_component(&ecs, crawler1_ent, POSITION_COMP_ID, &pos);
+    pos = (position_comp_t) {.x = 117.f, .y = 49.f};
+    ecs_add_component(&ecs, crawler2_ent, POSITION_COMP_ID, &pos);
 
-    velocity_comp_t vel = {.dx = 1.f, .dy = 1.f};
+    velocity_comp_t vel = {.dx = 0.f, .dy = 0.f};
     ecs_add_component(&ecs, player_ent, VELOCITY_COMP_ID, &vel);
-    vel = (velocity_comp_t) {.dx = 1.f, .dy = 1.f};
-    ecs_add_component(&ecs, enemy_ent, VELOCITY_COMP_ID, &vel);
-    vel = (velocity_comp_t) {.dx = 2.f, .dy = 2.f};
-    ecs_add_component(&ecs, asteroid1_ent, VELOCITY_COMP_ID, &vel);
-    vel = (velocity_comp_t) {.dx = 3.f, .dy = 3.f};
-    ecs_add_component(&ecs, asteroid2_ent, VELOCITY_COMP_ID, &vel);
-    ecs_add_component(&ecs, asteroid3_ent, VELOCITY_COMP_ID, &vel);
+    vel = (velocity_comp_t) {.dx = 0.f, .dy = 0.f};
+    ecs_add_component(&ecs, cauldron_ent, VELOCITY_COMP_ID, &vel);
+    ecs_add_component(&ecs, crawler1_ent, VELOCITY_COMP_ID, &vel);
+    ecs_add_component(&ecs, crawler2_ent, VELOCITY_COMP_ID, &vel);
 
-    sprite_comp_t sprite = {
-        .sprites = player_ship_bmp, .frame_size = 16, .frame_count = 3, .width = 10, .height = 8};
+    sprite_comp_t sprite = {.sprites = player_ship_bmp,
+                            .frame_size = PLAYER_SHIP_BMP_FRAME_SIZE,
+                            .frame_count = PLAYER_SHIP_BMP_FRAME_COUNT,
+                            .width = PLAYER_SHIP_BMP_FRAME_WIDTH,
+                            .height = PLAYER_SHIP_BMP_FRAME_HEIGHT};
     ecs_add_component(&ecs, player_ent, SPRITE_COMP_ID, &sprite);
-    sprite = (sprite_comp_t) {
-        .sprites = crawler_bmp, .frame_size = 30, .frame_count = 2, .width = 11, .height = 15};
-    ecs_add_component(&ecs, enemy_ent, SPRITE_COMP_ID, &sprite);
-    sprite = (sprite_comp_t) {.sprites = asteroid_big_bmp, .width = 13, .height = 12};
-    ecs_add_component(&ecs, asteroid1_ent, SPRITE_COMP_ID, &sprite);
-    sprite = (sprite_comp_t) {.sprites = asteroid_small_bmp, .width = 7, .height = 6};
-    ecs_add_component(&ecs, asteroid2_ent, SPRITE_COMP_ID, &sprite);
-    ecs_add_component(&ecs, asteroid3_ent, SPRITE_COMP_ID, &sprite);
+    sprite = (sprite_comp_t) {.sprites = cauldron_bmp,
+                              .frame_size = CAULDRON_BMP_FRAME_SIZE,
+                              .frame_count = CAULDRON_BMP_FRAME_COUNT,
+                              .width = CAULDRON_BMP_FRAME_WIDTH,
+                              .height = CAULDRON_BMP_FRAME_HEIGHT};
+    ecs_add_component(&ecs, cauldron_ent, SPRITE_COMP_ID, &sprite);
+    sprite = (sprite_comp_t) {.sprites = crawler_bmp,
+                              .frame_size = CRAWLER_BMP_FRAME_SIZE,
+                              .frame_count = CRAWLER_BMP_FRAME_COUNT,
+                              .width = CRAWLER_BMP_FRAME_WIDTH,
+                              .height = CRAWLER_BMP_FRAME_HEIGHT};
+    ecs_add_component(&ecs, crawler1_ent, SPRITE_COMP_ID, &sprite);
+    ecs_add_component(&ecs, crawler2_ent, SPRITE_COMP_ID, &sprite);
 }
 
 void game_system_move()
@@ -99,18 +101,10 @@ void game_system_move()
     for (int idx = 0; idx < component_count; ++idx) {
         position_comp_t* pos = ECS_GET_COMP_FROM_IDX(&ecs, POSITION_COMP_ID, idx, position_comp_t);
         ecs_entity_t ent = ecs.components[POSITION_COMP_ID].set.dense[idx];
-        sprite_comp_t* sp = ECS_GET_COMP_FROM_ENT(&ecs, SPRITE_COMP_ID, ent, sprite_comp_t);
         velocity_comp_t* vel = ECS_GET_COMP_FROM_ENT(&ecs, VELOCITY_COMP_ID, ent, velocity_comp_t);
 
-        // Bouncing off walls effect
         pos->x += vel->dx;
         pos->y += vel->dy;
-        if (pos->x > (OLED_WIDTH - sp->width) || pos->x < 0.f) {
-            vel->dx *= -1;
-        }
-        if (pos->y > (OLED_HEIGHT - sp->height) || pos->y < 0.f) {
-            vel->dy *= -1;
-        }
     }
 }
 
